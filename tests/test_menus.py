92f2e373ca541ea21fa9fa42bc184c9be9ff898e
Noah Hefner <noahhefner@users.noreply.github.com>
2020-08-19 15:11:16 -0400

    working on test classes

--- tests/test_menus.py
+++ tests/test_menus.py
@@ -1,3 +1,32 @@
-import unittest
+"""
+Noah Hefner
+Test Classes for nhefner_pygame_menus
+Last Edit: 19 August 2020
+"""
 
+# Imports
+import unittest
 from menus import *
+
+# Initialize pygame
+pygame.init()
+
+class TestButtonPicture (unittest.TestCase):
+
+    def setUp(self):
+
+        button = ButtonPicture()
+
+    def tearDown (self):
+
+        pass
+
+def suite():
+
+    suite = unittest.TestSuite()
+    return suite
+
+if __name__ == '__main__':
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
